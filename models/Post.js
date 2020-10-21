c8f0fd6e18653ee5d58cbbf1d8de7521fb85b0ee
natswatch <nathaliec@me.com>
2020-10-20 21:26:41 -0700

    comment routes

--- models/Post.js
+++ models/Post.js
@@ -5,7 +5,7 @@ const sequelize = require('../config/connection');
 class Post extends Model {
     static upvote(body, models){
         return models.Vote.create({
-            user_id: body.ueer_id,
+            user_id: body.user_id,
             post_id: body.post_id
         }).then(() => {
             return Post.findOne({
--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 const User = require('./User');
 const Post = require('./Post');
 const Vote = require('./Vote');
+const Comment = require('./Comment');
+
 
 
 //
@@ -40,5 +42,21 @@ Post.hasMany(Vote, {
     foreignKey: 'post_id'
 });
 
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
-module.exports = { User, Post, Vote };
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+
+module.exports = { User, Post, Vote, Comment };
